b604fa295cefa0c7d738d6721985eda8b1d34204
Yurka1111 <yuraaa1111@gmail.com>
2018-10-25 17:57:22 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int A =3*3+4*4+5*5+6*6+7*7+8*8+9*9;
+        System.out.print ("A = "+A);
+        int B= 2*3*4*5*6*7*8;
+        System.out.print ('\n');
+        System.out.print ("B = "+B);
+        System.out.print ('\n');
+        int C= A+B;
+        System.out.print ("C = " +C);
+
+    }
+}
